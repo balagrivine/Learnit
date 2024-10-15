5507794838263b77d580f41267b5b71e761d4315
balagrivine <balagrivine@gmail.com>
2024-10-14 21:18:12 -0700

    feat: create an api endpoint that allows user login

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,16 +1,24 @@
 from fastapi import APIRouter, HTTPException, status, Depends
 from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
 
 from src.database import get_db
 from src.auth import service
 from src.auth.schemas import UserAuth
+from src.models import User
 
 auth_router = APIRouter()
 
 @auth_router.post("/register", status_code=status.HTTP_201_CREATED)
 async def register_user(user_auth: UserAuth, db: AsyncSession=Depends(get_db)):
+    """Handles registration of users
+    Args:
+        user_auth: Schema containing user registration data
+    Returns:
+        Dict containing info of the newly created user
+    """
     try:
         user = await service.create_user(user_auth, db)
 
@@ -25,4 +33,32 @@ async def register_user(user_auth: UserAuth, db: AsyncSession=Depends(get_db)):
     except HTTPException as e:
         raise e
     except Exception as e:
+        raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="An error occured while registering users"
+                )
+
+@auth_router.post("/token", status_code=status.HTTP_200_OK)
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+        db: AsyncSession=Depends(get_db)):
+    """Handles login in of users
+    Args:
+        form_data: From containing user login data
+    Returns:
+        Dict containing info of the logged in user
+    """
+    try:
+        user = await service.login_user(form_data, db)
+
+        return JSONResponse(content=user)
+    except HTTPException as e:
         raise e
+    except Exception as e:
+        raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="An error occured while loggin in the user"
+                )
+
+@auth_router.get("/users/me", status_code=status.HTTP_200_OK)
+async def get_current_user(current_user: Annotated[User, Depends(service.get_current_user)]):
+    return current_user
--- src/auth/service.py
+++ src/auth/service.py
@@ -4,20 +4,21 @@ import jwt
 from jwt.exceptions import InvalidTokenError
 from passlib.context import CryptContext
 import os
-from typing import Dict
+from typing import Dict, Annotated
 from sqlalchemy.ext.asyncio import AsyncSession
-from datetime import datetime, UTC
+from datetime import datetime, UTC, timedelta
+from sqlalchemy import select
 from sqlalchemy.exc import StatementError
 
 from .schemas import UserAuth
 from src.models import User
+from src.database import get_db
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/token")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 ALGORITHM = os.getenv("ALGORITHM")
 SECRET_KEY = os.getenv("SECRET_KEY")
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 async def create_user(user: UserAuth, db: AsyncSession) -> User:
     """Creates a user in the database
@@ -45,3 +46,55 @@ async def create_user(user: UserAuth, db: AsyncSession) -> User:
         raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="An internal server error occured while creating the user")
+
+async def login_user(form_data, db: AsyncSession) -> Dict[any, str]:
+    try:
+        user = await get_user(form_data.username, db)
+
+        if not user or not pwd_context.verify(form_data.password, user.password):
+            raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Incorrect email or password",
+                    headers={"WWW_Authenticate":"Bearer"}
+                    )
+        access_token = create_access_token(data={"sub": user.email})
+        return {
+                "user_id": user.id,
+                "email": user.email,
+                "token_type": "bearer",
+                "access_token": access_token
+                }
+    except Exception as e:
+        raise e
+
+async def get_user(email: str, db: AsyncSession) -> User:
+    stmt = select(User).where(User.email==email)
+    result = await db.execute(stmt)
+    return result.scalar()
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(UTC) + timedelta(hours=2)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)],
+        db: AsyncSession=Depends(get_db)):
+    credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate":"Bearer"},
+            )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email: str = payload["sub"]
+        if email is None:
+            raise credentials_exception
+
+        user = await get_user(email, db)
+        if not user:
+            raise credentials_exception
+        return user
+    except InvalidTokenError:
+        raise credentials_exception
